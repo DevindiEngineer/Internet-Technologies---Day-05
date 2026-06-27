77718cbc30cfbb0ddbbc31d59c824c276105654d
DevindiEngineer <devindicampus@gmail.com>
2026-06-27 13:01:50 +0530

    data types

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
 
 console.log("Hello Java Script!");
+
 let name = "Devindi";
-console.log(name);
+
+
 let answer = "Yes";
 console.log(answer);
+
 let age=20;
 console.log(age);
+
+let question=false;
+console.log(answer);
+
+name = "Ranil";
+console.log(name);
+
+alert("Hello Guys!")
+let phone = "070092";
+console.log(phone);
 
 
